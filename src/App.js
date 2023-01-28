699ffe9ff161bf815c5e659cc040d0b7b85985b4
Carles Espuña <carlespuna@gmail.com>
2023-01-28 11:10:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import ErrorPage from './pages/ErrorPage';
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
+import PrivateView from './pages/PrivateView';
+import IsPrivate from './components/IsPrivate';
+
+function App() {
+   return (
+      <div className="App">
+         <Toaster />
+         <Navbar />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/private" element={<IsPrivate><PrivateView /></IsPrivate>} />
+            <Route path="*" element={<ErrorPage />} />
+         </Routes>
+      </div>
+   );
+}
+
+export default App;
