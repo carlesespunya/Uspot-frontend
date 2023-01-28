d0ca12d409da7c58581f45fcca2a6c623a03892f
carles espunya <carlespuna@gmail.com>
2023-01-28 12:12:51 +0100

    global data added

---
+++ src/data/globalData.js
@@ -0,0 +1,5 @@
+const types = ['Day', 'Trip', 'Tournament', 'Course'];
+const sports = ['Surf', 'Wind Surf', 'Paddle Surf', 'Kayak', 'Kite Surf', 'Scuba Diving', 'Mountain Skiing', 'Motocross', 'Cycling', 'Other'];
+const regions = ['Barcelona', 'Griona', 'Costa Brava', 'La Cerdanya', 'Illes Balears', 'Lleida', 'Tarragona', 'Andorra', 'Other']
+const bookingStatus = ["Accepted", "Pending", "Canceled"]
+const eventStatus = ['Open', 'Full', 'Started', 'Finished', 'Canceled']
