bb07a502944bbd66b5c9edd88e2f93774bc41165
carles espunya <carlespuna@gmail.com>
2023-01-30 11:30:22 +0100

    my profile / routes done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Home from './pages/home/index';
+import Home from './pages/Home/index';
 import Navbar from './components/Navbar';
 import ErrorPage from './pages/ErrorPage';
-import Signup from './pages/auth/Signup';
-import Login from './pages/auth/Login';
+import Signup from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
 import IsPrivate from './components/IsPrivate';
+import Bookings from './pages/bookings';
+import Experience from './pages/experience';
+import Experiences from './pages/experiences';
+import Profile from './pages/profile';
+import MyProfile from './pages/MyProfile';
+
 
 function App() {
    return (
@@ -17,7 +23,11 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/private" element={<IsPrivate></IsPrivate>} />
+            <Route path="/bookings" element={<IsPrivate><Bookings /></IsPrivate>} />
+            <Route path="/experience" element={<IsPrivate><Experience /></IsPrivate>} />
+            <Route path="/experiences" element={<Experiences />} />
+            <Route path="/profile" element={<IsPrivate><Profile /></IsPrivate>} />
+            <Route path="/myProfile" element={<IsPrivate><MyProfile /></IsPrivate>} />
             <Route path="*" element={<ErrorPage />} />
          </Routes>
       </div>
---
+++ src/pages/MyProfile/index.js
@@ -0,0 +1,10 @@
+import './MyProfile.css';
+import React from 'react';
+
+export default function MyProfile() {
+   return (
+      <>
+         <h1>My Profile</h1>
+      </>
+   )
+}
