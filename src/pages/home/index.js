472a1be4305f29e1d01004f2b3ff0d7c58ffeb44
carles espunya <carlespuna@gmail.com>
2023-01-30 13:07:28 +0100

    card and caarousel ui compoents created

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,8 @@
 import './Home.css';
 import React from 'react';
 import { sports, types, regions } from '../../data/globalData';
+import Card from "../../components/ui/Card";
+import Carousel from "../../components/ui/Carousel";
 
 export default function Home() {
    return (
@@ -12,37 +14,49 @@ export default function Home() {
             <div className="home-section">
                <h2 className="home-section-title">Discover our sports</h2>
                <div className="home-section-cards">
-                  {sports.map((sport) => {
-                     return (
-                        <div className="home-section-card">
-                           <h3>{sport}</h3>
-                        </div>
-                     )
-                  })}
+                  <Carousel>
+                     {sports.map((sport) => {
+                        return (
+                           <Card>
+                              <div className="home-section-card">
+                                 <h3>{sport}</h3>
+                              </div>
+                           </Card>
+                        )
+                     })}
+                  </Carousel>
                </div>
             </div>
             <div className="home-section">
                <h2 className="home-section-title">A wide variety of sport experiences</h2>
                <div className="home-section-cards">
-                  {types.map((sport) => {
-                     return (
-                        <div className="home-section-card">
-                           <h3>{sport}</h3>
-                        </div>
-                     )
-                  })}
+                  <Carousel>
+                     {types.map((type) => {
+                        return (
+                           <Card>
+                              <div className="home-section-card">
+                                 <h3>{type}</h3>
+                              </div>
+                           </Card>
+                        )
+                     })}
+                  </Carousel>
                </div>
             </div>
             <div className="home-section">
                <h2 className="home-section-title">Locations</h2>
                <div className="home-section-cards">
-                  {regions.map((sport) => {
-                     return (
-                        <div className="home-section-card">
-                           <h3>{sport}</h3>
-                        </div>
-                     )
-                  })}
+                  <Carousel>
+                     {regions.map((region) => {
+                        return (
+                           <Card>
+                              <div className="home-section-card">
+                                 <h3>{region}</h3>
+                              </div>
+                           </Card>
+                        )
+                     })}
+                  </Carousel>
                </div>
             </div>
          </div>
