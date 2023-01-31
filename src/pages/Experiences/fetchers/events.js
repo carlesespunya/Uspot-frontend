c389d259e76d8c17cbb7a53e58506c09f44b51a0
carles espunya <carlespuna@gmail.com>
2023-01-31 08:42:58 +0100

    startig with tanstack

--- src/pages/Experiences/fetchers/events.js
+++ src/pages/Experiences/fetchers/events.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export const fetchEvents = async (query) => {
-   console.log("Fetching events");
-   const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/events`, { params: query });
-   const events = response.data;
-
-   console.log("Events ", events);
-   return events;
+export default function fetchEvents() {
+   return axios.get(`http://localhost:8000/api/v1/events`);
 };
--- src/pages/Experiences/index.js
+++ src/pages/Experiences/index.js
@@ -1,10 +1,27 @@
 import './Experiences.css';
 import React from 'react'
+import { useQuery } from "@tanstack/react-query"
+import fetchEvents from "./fetchers/events"
 
 export default function Experiences() {
+   const { status, error, data} = useQuery({
+      queryKey: ["events"],
+      queryFn: fetchEvents,
+   })
+
+   if (status === "loading") return <h1>Loading...</h1>
+   if (status === "error") return <h1>{JSON.stringify(error)}</h1>
+
    return (
       <>
          <h1>Experiences</h1>
+         <ol>
+            {data.data.events.map(event => {
+               return(<li>
+                  {event.title}
+               </li>)
+            })}
+         </ol>
       </>
    )
 }
