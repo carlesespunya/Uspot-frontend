96cad76b486801ba98b9848ca49d1c8d70350d9a
carles espunya <carlespuna@gmail.com>
2023-02-01 13:08:24 +0100

    filter with params working

--- src/pages/Experiences/index.js
+++ src/pages/Experiences/index.js
@@ -1,5 +1,5 @@
 import './Experiences.css';
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from "@tanstack/react-query"
 import fetchEvents from "./fetchers/events"
 import { useSearchParams } from 'react-router-dom';
@@ -10,20 +10,26 @@ import FormattedDate from '../../utils/FormattedDate';
 
 export default function Experiences() {
    const [params, setParams] = useSearchParams()
-   const [selectedSport, setSelectedSport] = useState("Surf")
+   const [selectedSport, setSelectedSport] = useState()
 
    const handleSportChange = (e) => {
-      setSelectedSport(e.target.value);
       setParams((prevParams) => {
+         if (e.target.value === "All") {
+            const { sport, ...rest } = prevParams;
+            return rest;
+         }
          return { ...prevParams, sport: e.target.value };
       });
    }
-   const queryKey = useMemo(() => ["events", { sport: selectedSport }], [selectedSport]);
+
+   useEffect(() => {
+      if (params.has("sport")) setSelectedSport(params.get("sport"))
+   }, [params])
 
    const { status, error, data } = useQuery({
-      queryKey,
+      queryKey: ["events", { sport: params.get("sport") }],
       keepPreviousData: true,
-      queryFn: () => fetchEvents({sport: selectedSport})
+      queryFn: () => fetchEvents(params)
    })
 
    if (status === "loading") return <h1>Loading...</h1>
@@ -34,20 +40,22 @@ export default function Experiences() {
       <div className="experiences">
          <h1 className="experiences-title">U-Experiences</h1>
          <div className="experiences-search">
-            <div className="experiences-filter">
-               <h2>Filter</h2>
-               <div className="sport-filter">
-                  Sport
-                  <select value={selectedSport} onChange={handleSportChange}>
-                        <option value="">All</option>
-                        {sports.data.map((sport) => (
-                           <option key={sport.key} value={sport.key}>
-                              {sport.name}
-                           </option>
-                        ))}
-                     </select>
-               </div>
+
+         </div>
+         <div className="experiences-filter">
+            <h2>Filter</h2>
+            <div className="sport-filter">
+               Sport
+               <select value={selectedSport} onChange={handleSportChange}>
+                  <option value="All">All</option>
+                  {sports.data.map((sport) => (
+                     <option key={sport.key} value={sport.key}>
+                        {sport.name}
+                     </option>
+                  ))}
+               </select>
             </div>
+
          </div>
          <div className="experiences-list">
             <div className="experiences-list-box">
@@ -65,7 +73,7 @@ export default function Experiences() {
                                  </div>
                               </div>
                               <div className="experiences-list-card-up-top">
-                                 <div className="experiences-list-card-user-img" style={{ backgroundImage: `url(${ event.user.imageUrl })` }}>
+                                 <div className="experiences-list-card-user-img" style={{ backgroundImage: `url(${event.user.imageUrl})` }}>
                                  </div>
                                  <div className="experiences-list-card-info experiences-status-card" style={{ backgroundColor: eventStatus.data.find(status => status.key === event.status).color }}>
                                     {eventStatus.data.find(status => status.key === event.status).name}
@@ -73,7 +81,7 @@ export default function Experiences() {
                               </div>
                            </div>
                            <div className="experiences-list-card-down">
-                              <h3 className="experiences-list-card-title">{ event.title }</h3>
+                              <h3 className="experiences-list-card-title">{event.title}</h3>
                               <p>Start Date: {FormattedDate(event.startDate)}</p>
                               <p>End Date: {FormattedDate(event.endDate)}</p>
                               <div className="experiences-list-card-down-bot">
