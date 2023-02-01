47b9cdba9fbda26db90d61e95966fa2ec8dd7eac
carles espunya <carlespuna@gmail.com>
2023-02-01 14:45:44 +0100

    links added to homepage

--- src/pages/home/HomeSectionCard.js
+++ src/pages/home/HomeSectionCard.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Card from "../../components/ui/Card";
 
 export default function HomeSectionCard({ option }) {
    return (
-      <Card>
-         <div className="home-section-card" style={{
-            backgroundImage: `url(${option.img})`,
-            backgroundColor: option.color
-         }}>
-            <h3 className="home-section-card-title">{option.name}</h3>
-         </div>
-      </Card>
+      <Link to={`/experiences?status=Open&${option.group}=${option.key}`}>
+         <Card>
+            <div className="home-section-card" style={{
+               backgroundImage: `url(${option.img})`,
+               backgroundColor: option.color
+            }}>
+               <h3 className="home-section-card-title">{option.name}</h3>
+            </div>
+         </Card>
+      </Link>
    );
 }
